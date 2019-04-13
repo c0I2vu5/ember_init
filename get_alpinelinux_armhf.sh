c17f546c6f119a016c356de91d68d19c6d8b3e80
Eisen-Fluegel <lynx.mondlicht@gmail.com>
2019-04-14 03:45:51 +0900

    Add init setting script

--- get_alpinelinux_armhf.sh
+++ get_alpinelinux_armhf.sh
@@ -6,4 +6,5 @@ mkdir zw
 tar zxf alpine*.tar.gz -C zw
 rm -f alpine*.tar.gz
 echo "enable_uart=1" > ./zw/usercfg.txt
+echo "microSD have to be formatted with FAT32 bootable."
 echo "exec : cp -arp ./zw/* /path/to/microSD/root"
---
+++ init.sh
@@ -0,0 +1,68 @@
+#!/bin/ash
+
+# SETUP
+## Init
+###login with root nopassword
+
+setup-alpine
+#layout               jp
+#variant              jp
+#hostname             ember
+#interface eth0       [enter]
+#ip address for eth0  [enter]
+#interface wlan0      [enter]
+#wireless network     $SSID
+#Pre-shared key       $WLANPASS
+#ip address for wlan0 [enter]
+#network conf         [enter]
+#root password        $[PASS]
+#timezone             Japan
+#proxy                [enter]
+#ntp client           [enter]
+#download mirror      [enter]
+#ssh server           [none]
+#store configs        [enter]
+#apk cache            [enter]
+
+sed -i /etc/apk/repositories
+    -e '/v3.*\/community/s@^#@@'
+apk update
+apk upgrade
+apk add --no-cache awake openrc util-linux
+
+# Main script
+cat > /etc/init.d/ignite <<EOF
+#!/sbin/openrc-run
+
+description="when be activated this script, wake-on-lan hardcoding target."
+
+depend() {
+    need wpa_supplicant
+}
+
+start() {
+    ebegin "Starting ignite"
+    ping -c 1 $tgt_ip > /dev/null
+    while [ $? -eq 1 ]
+    do
+        awake $tgt_mac
+        sleep 1m
+        ping -c 1 $tgt_ip > /dev/null
+    done
+}
+
+stop() {
+    ebegin "Stopping ignite"
+    pkill -9 -x ignite
+}
+EOF
+chmod +x /etc/init.d/ignite
+
+# Set auto-start service
+rc-update add ignite default
+rc-update add wpa_supplicant boot
+
+# Commit changes
+lbu include /etc/init.d/ignite
+lbu commit -d
+reboot
