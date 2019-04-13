639657ee38b0c5332e1f63d1a1a9386c67a11f82
Eisen-Fluegel <lynx.mondlicht@gmail.com>
2019-04-14 03:39:22 +0900

    Add get OS script

---
+++ get_alpinelinux_armhf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf ./zw
+curl -LO http://dl-cdn.alpinelinux.org/alpine/v3.9/releases/armhf/alpine-rpi-3.9.2-armhf.tar.gz
+mkdir zw
+tar zxf alpine*.tar.gz -C zw
+rm -f alpine*.tar.gz
+echo "enable_uart=1" > ./zw/usercfg.txt
+echo "exec : cp -arp ./zw/* /path/to/microSD/root"
